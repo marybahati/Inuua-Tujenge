46bd2af36a8d164de55c44d3f863de81808f690a
gracekimaniwairimu <gracewairimukim@gmail.com>
2020-07-04 15:36:06 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import house from './Assets/house.jpg';
+
+function App() {
+  return (
+    <div style={{maxHeight: 500}}>
+    <div className="container-fluid p-0">
+        <img src='https://danmarhomes.com/wp-content/uploads/2018/12/1920X1080_Waterford.jpg' class="img-fluid" />
+        
+    </div>
+    </div>
+  );
+}
+
+export default App;
